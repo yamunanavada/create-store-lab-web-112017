977182ed489beff9d1669c6bed18c7b39c1d0022
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 13:14:37 -0500

    fix syntax erros in index-test.js

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
-import sinon from 'sinon'
-import { shallow, mount } from 'enzyme'
-import React from 'react'
-import UserInput from '../src/components/UserInput'
-import { configureStore } from '../src/index.js'
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import UserInput from '../src/components/UserInput';
+import { configureStore } from '../src/index.js';
 
 describe('store', () => {
   
