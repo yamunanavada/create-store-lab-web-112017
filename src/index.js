b57460f6d17d3c1aa823b62f0ac0e2a451acfd32
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 13:11:50 -0500

    remove unneeded whitespace in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import { createStore } from 'redux';
 import manageBand from './reducers/manageBand';
 import App from './App';
 
-
 // we wrap store in a function for testing purposes
 export function configureStore(){
-  
+  return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 };
 
 ReactDOM.render(
